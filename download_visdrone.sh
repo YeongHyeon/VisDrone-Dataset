4f487a576904a8cb2dd9fdb17d9c54549d1ea9af
YeongHyeon Park <young200405@gmail.com>
2025-01-21 15:53:15 +0900

    download script of whole dataset is added

---
+++ download_visdrone.sh
@@ -0,0 +1,29 @@
+# Task 1: Object Detection in Images
+gdown https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn -O 1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn.zip && unzip 1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn.zip
+gdown https://drive.google.com/uc?id=1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59 -O 1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59.zip && unzip 1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59.zip
+gdown https://drive.google.com/uc?id=1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V -O 1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V.zip && unzip 1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V.zip
+gdown https://drive.google.com/uc?id=1KN8R3oioOvSXH492GEVk-Hx74nWHAcXT -O 1KN8R3oioOvSXH492GEVk-Hx74nWHAcXT.zip && unzip 1KN8R3oioOvSXH492GEVk-Hx74nWHAcXT.zip
+
+# Task 2: Object Detection in Videos
+gdown https://drive.google.com/uc?id=1NSNapZQHar22OYzQYuXCugA3QlMndzvw -O 1NSNapZQHar22OYzQYuXCugA3QlMndzvw.zip && unzip 1NSNapZQHar22OYzQYuXCugA3QlMndzvw.zip
+gdown https://drive.google.com/uc?id=1xuG7Z3IhVfGGKMe3Yj6RnrFHqo_d2a1B -O 1xuG7Z3IhVfGGKMe3Yj6RnrFHqo_d2a1B.zip && unzip 1xuG7Z3IhVfGGKMe3Yj6RnrFHqo_d2a1B.zip
+gdown https://drive.google.com/uc?id=1-BEq--FcjshTF1UwUabby_LHhYj41os5 -O 1-BEq--FcjshTF1UwUabby_LHhYj41os5.zip && unzip 1-BEq--FcjshTF1UwUabby_LHhYj41os5.zip
+gdown https://drive.google.com/uc?id=1Qwyp_cEpGyXGqJ8IbusEzuNHgbM403NP -O 1Qwyp_cEpGyXGqJ8IbusEzuNHgbM403NP.zip && unzip 1Qwyp_cEpGyXGqJ8IbusEzuNHgbM403NP.zip
+
+# Task 3: Single-Object Tracking
+gdown https://drive.google.com/uc?id=1a-AmQjYATzj8seXLlEm9Sx8aQmClrJka -O 1a-AmQjYATzj8seXLlEm9Sx8aQmClrJka.zip && unzip 1a-AmQjYATzj8seXLlEm9Sx8aQmClrJka.zip
+gdown https://drive.google.com/uc?id=16YPyhNDQrTgW8I2HaH_HNEO-KTRA-xso -O 16YPyhNDQrTgW8I2HaH_HNEO-KTRA-xso.zip && unzip 16YPyhNDQrTgW8I2HaH_HNEO-KTRA-xso.zip
+gdown https://drive.google.com/uc?id=18SNAOlCJtApnG2m45ud-1e_OtGYill0D -O 18SNAOlCJtApnG2m45ud-1e_OtGYill0D.zip && unzip 18SNAOlCJtApnG2m45ud-1e_OtGYill0D.zip
+gdown https://drive.google.com/uc?id=1xCiHjU4JlR9QsYtiHYy2UUd3m6NthoBC -O 1xCiHjU4JlR9QsYtiHYy2UUd3m6NthoBC.zip && unzip 1xCiHjU4JlR9QsYtiHYy2UUd3m6NthoBC.zip
+gdown https://drive.google.com/uc?id=1zxMLZrkkz4BkufbaDXQVMpDz-PRpPpxs -O 1zxMLZrkkz4BkufbaDXQVMpDz-PRpPpxs.zip && unzip 1zxMLZrkkz4BkufbaDXQVMpDz-PRpPpxs.zip
+gdown https://drive.google.com/uc?id=13J_9rwpf2TpVVsWgzMUv4uCxpOKDNbef -O 13J_9rwpf2TpVVsWgzMUv4uCxpOKDNbef.zip && unzip 13J_9rwpf2TpVVsWgzMUv4uCxpOKDNbef.zip
+gdown https://drive.google.com/uc?id=1z7lQ0co8Dcu-ZojMN7dnIxn-i6QbGNL4 -O 1z7lQ0co8Dcu-ZojMN7dnIxn-i6QbGNL4.zip && unzip 1z7lQ0co8Dcu-ZojMN7dnIxn-i6QbGNL4.zip
+
+# Task 4: Multi-Object Tracking
+gdown https://drive.google.com/uc?id=1-qX2d-P1Xr64ke6nTdlm33om1VxCUTSh -O 1-qX2d-P1Xr64ke6nTdlm33om1VxCUTSh.zip && unzip 1-qX2d-P1Xr64ke6nTdlm33om1VxCUTSh.zip
+gdown https://drive.google.com/uc?id=1rqnKe9IgU_crMaxRoel9_nuUsMEBBVQu -O 1rqnKe9IgU_crMaxRoel9_nuUsMEBBVQu.zip && unzip 1rqnKe9IgU_crMaxRoel9_nuUsMEBBVQu.zip
+gdown https://drive.google.com/uc?id=14z8Acxopj1d86-qhsF1NwS4Bv3KYa4Wu -O 14z8Acxopj1d86-qhsF1NwS4Bv3KYa4Wu.zip && unzip 14z8Acxopj1d86-qhsF1NwS4Bv3KYa4Wu.zip
+gdown https://drive.google.com/uc?id=1I0nn6dVKctzDE5YJ3q9qOlhKLiSIDAxF -O 1I0nn6dVKctzDE5YJ3q9qOlhKLiSIDAxF.zip && unzip 1I0nn6dVKctzDE5YJ3q9qOlhKLiSIDAxF.zip
+
+# Task 5: Crowd Counting
+gdown https://drive.google.com/uc?id=1HY3V4QObrVjzXUxL_J86oxn2bi7FMUgd -O 1HY3V4QObrVjzXUxL_J86oxn2bi7FMUgd.zip && unzip 1HY3V4QObrVjzXUxL_J86oxn2bi7FMUgd.zip
